bba869a0aa4440cb1665503de279ea01406ede79
James Kristin <james.kristin2000@gmail.com>
2022-11-25 23:51:59 -0500

    Completed Exercise_DepartmentDAO

--- java/module-2/week-3/01_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcDepartmentDao.java
+++ java/module-2/week-3/01_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcDepartmentDao.java
@@ -20,17 +20,50 @@ public class JdbcDepartmentDao implements DepartmentDao {
 
 	@Override
 	public Department getDepartment(int id) {
-		return new Department(0, "Not Implemented Yet");
+		Department department = null;
+		String sql = "SELECT department_id, name " +
+				"FROM department " +
+				"WHERE department_id = ?;";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+		if (results.next()) {
+			department = mapRowToDepartment(results);
+		}
+		return department;
 	}
 
 	@Override
 	public List<Department> getAllDepartments() {
-		return new ArrayList<>();
+		List<Department> departments = new ArrayList<>();
+		String sql = "SELECT department_id, name FROM department ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+		while (results.next()) {
+			departments.add(mapRowToDepartment(results));
+		}
+		return departments;
 	}
 
 	@Override
 	public void updateDepartment(Department updatedDepartment) {
-
+		Department department = updatedDepartment;
+		String updateDepartmentSql = "UPDATE department SET department_id = ?, name = ? WHERE department_id = ?";
+		int numberOfRowsUpdated = jdbcTemplate.update(updateDepartmentSql, updatedDepartment.getId(), updatedDepartment.getName(), updatedDepartment.getId());
+		if (numberOfRowsUpdated == 1) {
+			System.out.println(department);
+		}
+		else {
+			System.out.println("Update Department Failed");
+		}
 	}
+
+	private Department mapRowToDepartment(SqlRowSet rowSet) {
+		Department department = new Department();
+
+		department.setId(rowSet.getInt("department_id"));
+
+		department.setName(rowSet.getString("name"));
+
+		return department;
+	}
+
 
 }

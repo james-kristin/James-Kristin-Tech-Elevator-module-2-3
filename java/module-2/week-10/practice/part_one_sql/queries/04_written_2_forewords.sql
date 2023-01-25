7fe58692b33c676e79b5b017ba56032dfcd5894f
James Kristin <james.kristin2000@gmail.com>
2023-01-25 14:17:40 -0500

    COMPLETED part_one_sql Practice

--- java/module-2/week-10/practice/part_one_sql/queries/04_written_2_forewords.sql
+++ java/module-2/week-10/practice/part_one_sql/queries/04_written_2_forewords.sql
@@ -2,3 +2,10 @@
 -- Include the count of forewords written by each person (name the column 'foreword_count').
 -- Order by full_name ascending.
 -- (7 rows)
+
+SELECT p.first_name || ' ' || p.last_name AS full_name, count(*) AS foreword_count
+FROM person p
+JOIN book b ON p.person_id = b.foreword_by
+GROUP BY full_name
+HAVING count(*) >= 2
+ORDER BY full_name;

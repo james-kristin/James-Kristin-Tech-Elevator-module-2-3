d6ec7622bdb1bc492501cfc2d0bf6f83e01debeb
James Kristin <james.kristin2000@gmail.com>
2023-03-15 15:25:25 -0400

    Complete Tutorial_01 Week6

--- java/module-3/week-6/01_Intro_to_Vue_JS/tutorial/src/App.vue
+++ java/module-3/week-6/01_Intro_to_Vue_JS/tutorial/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="todo-app">
-
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
+import TodoList from './components/TodoList.vue';
 
 
 export default {
   name: 'app',
   components: {
+    TodoList
 
   }
 }

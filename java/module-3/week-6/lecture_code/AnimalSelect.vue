ad32838ecb12c82f2018f5d70f599938db4b3276
Dahnaya Joyner <dahnaya.joyner@techelevator.com>
2023-03-18 08:47:39 -0400

    lecture code

---
+++ java/module-3/week-6/lecture_code/AnimalSelect.vue
@@ -0,0 +1,33 @@
+// The select options should be each animal's name
+// When the animal is clicked, the emoji should display next to it
+
+<template>
+  <div>
+      <select>
+          <option>Please Select</option>
+          <option></option>
+      </select>
+      <span></span>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            animals: [
+                {name: 'Dog', emoji: '🐶'},
+                {name: 'Cat', emoji: '🐱'},
+                {name: 'Frog', emoji: '🐸'}
+            ]
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+span {
+    font-size: 3em;
+}
+</style>
---
+++ java/module-3/week-6/lecture_code/SwitchBox.vue
@@ -0,0 +1,41 @@
+<!-- This one is more fun if you add a few hundred instances of it with a v-for -->
+// Add multiple boxes
+// When an input is checked for that box, only that box color should be red
+
+<template>
+  <div :class="{red: isRed}">
+      <input type="checkbox" v-model="isRed" />
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isRed: false
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+* {
+    box-sizing: border-box;
+}
+
+div {
+    width: 50px;
+    height: 50px;
+    background-color: skyblue;
+    padding-top: 15px;
+    display: inline-block;
+    text-align: center;
+}
+
+.red {
+    background-color: firebrick;
+}
+
+</style>

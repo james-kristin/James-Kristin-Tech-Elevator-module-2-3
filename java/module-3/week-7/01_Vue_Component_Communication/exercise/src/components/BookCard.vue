0db879f43dc290d192a0aeb20b8f8bbe5c01205b
James Kristin <james.kristin2000@gmail.com>
2023-03-24 00:27:32 -0400

    Completed Exercise_01_week7

--- java/module-3/week-7/01_Vue_Component_Communication/exercise/src/components/BookCard.vue
+++ java/module-3/week-7/01_Vue_Component_Communication/exercise/src/components/BookCard.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="card">
-
+  <div class="card" v-bind:class="{ 'read' : book.read }">
+      <h2 class="book-title">{{book.title}}</h2>
+      <h3 class="book-author">{{book.author}}</h3>
+      <img class="book-image" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'"/>
+      <button class="btn" v-bind:class=" book.read ? 'mark-unread': 'mark-read' " v-on:click="toggleRead">{{readButtonStatus}}</button>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'book-card'
+    name: 'book-card',
+    props: ['book'],
+    methods: {
+        toggleRead() {
+            this.$store.commit('FLIP_READ', this.book);
+        }
+    },
+    computed: {
+        readButtonStatus() {
+            if (this.book.read) {
+                return "Mark Unread";
+            } else {
+                return "Mark Read";
+            }
+        }
+    }
 }
 </script>
 
---
+++ java/module-3/week-7/01_Vue_Component_Communication/exercise/src/components/NewBookForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <form class="new-book-form" v-on:submit.prevent="saveBook">
+      <input type="text" placeholder="Title" class="title-input" v-model="newBook.title">
+      <input type="text" placeholder="Author" class="author-input" v-model="newBook.author">
+      <input type="text" placeholder="ISBN" class="isbn-input" v-model="newBook.isbn">
+      <button>Save</button>
+  </form>
+</template>
+
+<script>
+export default {
+    name: 'new-book-form',
+    data() {
+        return {
+            newBook: {
+            title: '',
+            author: '',
+            read: false,
+            isbn: ''
+            }
+        }
+    },
+    methods: {
+        saveBook() {
+            this.newBook.read = false;
+            this.$store.commit('ADD_NEW_BOOK', this.newBook);
+            this.newBook = {
+                title: '',
+                author: '',
+                read: false,
+                isbn: ''
+            };
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>

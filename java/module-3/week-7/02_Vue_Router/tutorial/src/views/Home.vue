71504aead0ba4ad95e4d9afc1b8c2b91af64491b
James Kristin <james.kristin2000@gmail.com>
2023-03-24 22:17:35 -0400

    Completed Tutorial_02_week7

---
+++ java/module-3/week-7/02_Vue_Router/tutorial/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hi!</h1>
+    <p>My name is James and I am learning how to code in VueJS</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+
+}
+</script>
+
+<style>
+
+</style>
